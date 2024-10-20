acbfe820d14c12d425eca3800e066b27125530d8
Alfie Atkinson <alf.atkinson13@gmail.com>
2024-10-20 12:24:34 +0100

    Defined structure learning method with bnlearn implementation.

---
+++ utils/structure_learning.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import bnlearn
+
+def learn_structure(train_data : pd.DataFrame, use_bnlearn : bool = False, methodtype : str = 'hc', scoretype : str = 'bic'):
+    """Learns the structure of a Bayesian Network given training data.
+
+    Parameters:
+        train_data (pd.DataFrame): The training data to learn the structure of.
+        use_bnlearn (bool, optional): Whether to use bnlearns structure learning or self-implemented methods. Defaults to False.
+        methodtype (str, optional): The method of structure learning to use. Defaults to 'hc'.
+        scoretype (str, optional): The scoring method to use. Defaults to 'bic'.
+    """
+    if use_bnlearn:
+        return bnlearn.structure_learning.fit(train_data, methodtype=methodtype, scoretype=scoretype)
+    
+    return None #TODO - Implement own structure learning algorithms.
