71bad6c7cca37f8ed01f014fd488d1a6e493bcbe
Alfie Atkinson <alf.atkinson13@gmail.com>
2024-11-10 15:06:22 +0000

    Removed unnecessary parameters from discrete_cross_validation method

--- utils/train.py
+++ utils/train.py
@@ -1,17 +1,15 @@
 import pandas as pd 
-from typing import Callable, Any
+import numpy as np 
 from sklearn.model_selection import KFold
-from sklearn.metrics import metrics
+from sklearn.metrics import accuracy_score, log_loss, mean_squared_error
 
-def discrete_cross_validation(df: pd.DataFrame, structure_fn: Callable[..., Any], parameter_fn: Callable[..., Any], nsplits: int = 5, structure_kwargs: dict = None, parameter_kwargs: dict = None) -> tuple:
+def discrete_cross_validation(df: pd.DataFrame, nsplits: int = 5, structure_kwargs: dict = None, parameter_kwargs: dict = None) -> tuple:
     """
     Cross-validate a model by splitting data into training and test sets,
     applying structure learning and then parameter learning with bnlearn.
     
-    Parameters:
+    Parameter:
         df: The dataset to be used for cross-validation (pandas DataFrame).
-        structure_fn: The structure learning function from bnlearn.
-        parameter_fn: The parameter learning function from bnlearn.
         nsplits: The number of splits for K-fold cross-validation (default 5).
         structure_kwargs: Keyword arguments for the structure learning function.
         parameter_kwargs: Keyword arguments for the parameter learning function.
@@ -21,4 +19,3 @@ def discrete_cross_validation(df: pd.DataFrame, structure_fn: Callable[..., Any]
             - The trained model after the last fold.
             - A dictionary of average metrics across all folds.
     """
-    pass
